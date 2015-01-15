3083c460c668fa429337b638afba61e95ab613a0
ramana <sandhya@carmatec.com>
2015-01-15 15:36:06 +0530

    mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,6 +96,15 @@ RpSkypool::Application.configure do
 #    :authentication       => 'plain',
 #    :enable_starttls_auto => true }
 
-
+config.action_mailer.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+      :address        => 'smtp.sendgrid.net',
+      :port           => '587',
+      :authentication => :plain,
+      :user_name      => ENV['SENDGRID_USERNAME'],
+      :password       => ENV['SENDGRID_PASSWORD'],
+      :domain         => 'heroku.com',
+      :enable_starttls_auto => true
+  }
 
 end
