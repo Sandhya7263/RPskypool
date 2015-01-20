a62d142d58484052bde973bfa1feb1e2c9ced6a3
ramana <sandhya@carmatec.com>
2015-01-20 13:36:17 +0530

    changes in mailer.rb

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,9 @@
 class UserMailer < ActionMailer::Base
-  default from: "info@rpskypool.com"
+#  default from: "info@rpskypool.com"
 
   def customer_info(contact)
     @contact = contact
-    mail(to: "dsanju5652@gmail.com",cc: "rpchowdary85@gmail.com", subject: 'Customer Enquire')
+    mail(:from => "info@rpskypool.com", to =>  "dsanju5652@gmail.com", subject => "Customer Enquire")
   end
   
 end
